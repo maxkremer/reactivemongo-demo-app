73164bd957c497155636f65df8a07510ad4f788c
maxkremer <mkremer@datastay.com>
2013-07-08 17:14:42 -0400

    added new route for POST

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -18,26 +18,34 @@ import play.modules.reactivemongo.json.collection.JSONCollection
 import models.Article
 import models.Article._
 
-object Articles extends Controller with MongoController {
-  // get the collection 'articles'
+object Experience extends Controller with MongoController {
+  // get the collection 'experience'
   val collection = db[JSONCollection]("articles")
-  // a GridFS store named 'attachments'
+  
   
 
-  def index = Action { implicit request =>
+  def listall = Action { implicit request =>
     Async {
 
-      //Pass collection empty Json obj to do select * and get the cursor
+      //Pass empty collection  Json obj to do select * and get the cursor
       val cursor: Cursor[JsObject] = collection.find(Json.obj()).cursor[JsObject]
       //convert cursor to future list of jsobject
-      val futureArticleList: Future[List[JsObject]] = cursor.toList
+      val futureExperienceList: Future[List[JsObject]] = cursor.toList
       //convert list to jsArray so we get an jsArray of JsObjects
-      val futureArticleListJsonArray: Future[JsArray] = futureArticleList.map { aListOfJsonObjs =>  JsArray(aListOfJsonObjs)  }
+      val futureExperienceListJsonArray: Future[JsArray] = futureExperienceList.map { aListOfJsonObjs =>  JsArray(aListOfJsonObjs)  }
       
       //Apply map function to jsArray to turn json into an HTTP OK response with the JSON in the body
-      futureArticleListJsonArray.map { jsArrayArticles =>    Ok(jsArrayArticles)    }
+      futureExperienceListJsonArray.map { jsArray =>    Ok(jsArray)    }
 
     }
   }
+  
+  def addNew = Action { implicit request =>
+	    Async {
+	      val json: Option[JsValue] = request.body.asJson
+	      
+	      collection.insert(json).map{ _ => Redirect(routes.Experience.listall) }
+	    }
+    }
 
 }
